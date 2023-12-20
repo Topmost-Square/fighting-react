fcc50219735146794fdd2cc6963664432168a466
keithchasen <keithchasen89@gmail.com>
2023-12-20 20:31:22 +0100

    basic set up react app/ router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fight } from './pages/Fight';
+import { Main } from './pages/Main';
+import { Select } from './pages/Select';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/select" element={<Select />} />
+        <Route path="/fight" element={<Fight />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Fight.js
@@ -0,0 +1,5 @@
+export const Fight = () => {
+    return (
+        <div>Fight</div>
+    )
+}
